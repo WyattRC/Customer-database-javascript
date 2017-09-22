3825f7ea39cca60ad2046cf6f49de33cdb24fb89
Wyatt Currie <wcnd2016@gmail.com>
2017-09-22 01:23:53 -0400

    styled

--- the-easy-way.js
+++ the-easy-way.js
@@ -1,28 +1,28 @@
 console.log('HELLO WORLD')
 for (let i = 0; i < customers.results.length; i++) {
-console.log(customers.results[i].name.first)
+  console.log(customers.results[i].name.first)
   // document.querySelector('div').innerHTML = okok;
   let body = document.querySelector('body');
   let test = document.createElement('div');
-console.log(test)
+  console.log(test)
   let okok = `
-  <article>
+
   <div class='all'>
   <div class="pic">
-  <img src = ${customers.results[i].picture.medium} alt = "face">
+  <img src = ${customers.results[i].picture.large} alt = "face">
   </div>
   <div class='info'>
-  <p>${customers.results[i].name.first} ${customers.results[i].name.last}</p>
-  <p>${customers.results[i].email}</p>
-  <p>${customers.results[i].location.street}</p>
-  <p>${customers.results[i].location.city} ${customers.results[i].location.state} ${customers.results[i].location.postcode}</p>
-  <p>${customers.results[i].phone}</p>
-  <p>${customers.results[i].id.value}</p>
+  <h3>${customers.results[i].name.first} ${customers.results[i].name.last}</h3>
+  <p class='invis'>${customers.results[i].email}</p>
+  <p class ='greytext'>${customers.results[i].location.street}</p>
+  <p class='greytext'>${customers.results[i].location.city} ${customers.results[i].location.state} ${customers.results[i].location.postcode}</p>
+  <p class='greytext'>${customers.results[i].phone}</p>
+  <p class='ssn'>${customers.results[i].id.value}</p>
   </div>
   </div>
-  </article>
+  
   `;
-console.log(okok)
+  console.log(okok)
   test.innerHTML += okok;
   body.appendChild(test)
 }
